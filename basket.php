77154a20408890088dccc0cadb790f06eddf10a8
Alexander <shagov25@gmail.com>
2024-10-31 01:42:35 +0300

    завершил рефакторин по пунктам из задания

--- basket.php
+++ basket.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 const OPERATION_EXIT = 0;
 const OPERATION_ADD = 1;
 const OPERATION_DELETE = 2;
@@ -13,68 +14,119 @@ $operations = [
 
 $items = [];
 
+$isItemsEmpty = false;
 
 do {
-    system('clear');
-//    system('cls'); // windows
+   // system('clear');
+   system('cls'); // windows
 
-    do {
-        if (count($items)) {
-            echo 'Ваш список покупок: ' . PHP_EOL;
-            echo implode("\n", $items) . "\n";
-        } else {
-            echo 'Ваш список покупок пуст.' . PHP_EOL;
-        }
-
-
-        echo 'Выберите операцию для выполнения: ' . PHP_EOL;
-        // Проверить, есть ли товары в списке? Если нет, то не отображать пункт про удаление товаров
-        echo implode(PHP_EOL, $operations) . PHP_EOL . '> ';
-        $operationNumber = trim(fgets(STDIN));
-
-        if (!array_key_exists($operationNumber, $operations)) {
-            system('clear');
-
-            echo '!!! Неизвестный номер операции, повторите попытку.' . PHP_EOL;
-        }
-
-    } while (!array_key_exists($operationNumber, $operations));
-
-    echo 'Выбрана операция: '  . $operations[$operationNumber] . PHP_EOL;
+    $operationNumber = ShowStartMenu($items, $operations, $isItemsEmpty);
+    if(!CheckOperationNumber($operationNumber, $operations)) {
+        continue;
+    }
 
     switch ($operationNumber) {
         case OPERATION_ADD:
-            echo "Введение название товара для добавления в список: \n> ";
-            $itemName = trim(fgets(STDIN));
-            $items[] = $itemName;
+            AddItem($items, $isItemsEmpty);
             break;
-
         case OPERATION_DELETE:
-            // Проверить, есть ли товары в списке? Если нет, то сказать об этом и попросить ввести другую операцию
-            echo 'Текущий список покупок:' . PHP_EOL;
-            echo 'Список покупок: ' . PHP_EOL;
-            echo implode("\n", $items) . "\n";
-
-            echo 'Введение название товара для удаления из списка:' . PHP_EOL . '> ';
-            $itemName = trim(fgets(STDIN));
-
-            if (in_array($itemName, $items, true) !== false) {
-                while (($key = array_search($itemName, $items, true)) !== false) {
-                    unset($items[$key]);
-                }
-            }
+            DeleteItem($items,$isItemsEmpty);
             break;
-
         case OPERATION_PRINT:
-            echo 'Ваш список покупок: ' . PHP_EOL;
-            echo implode(PHP_EOL, $items) . PHP_EOL;
-            echo 'Всего ' . count($items) . ' позиций. '. PHP_EOL;
-            echo 'Нажмите enter для продолжения';
-            fgets(STDIN);
+            ShowShoppingList($items, $isItemsEmpty);
+            break;
+        default:
+
             break;
     }
 
     echo "\n ----- \n";
-} while ($operationNumber > 0);
+} while ($operationNumber !== "0");
 
 echo 'Программа завершена' . PHP_EOL;
+
+function ShowShoppingList ($items, $isItemsEmpty): void
+{
+    if($isItemsEmpty) {
+        echo 'Ваш список покупок пуст.' . PHP_EOL;
+        return;
+    }
+    echo "Ваш список покупок: " . PHP_EOL;
+    foreach ($items as $item) {
+        echo "\t" . $item . PHP_EOL;
+    }
+    echo 'Всего ' . count($items) . ' позиций. '. PHP_EOL;
+    echo 'Нажмите enter для продолжения';
+    fgets(STDIN);
+}
+
+function ShowStartMenu ($items ,$operations, &$isItemsEmpty): string
+{
+    // Проверить, есть ли товары в списке? Если нет, то не отображать пункт про удаление товаров
+    SetIsItemsEmpty($items, $isItemsEmpty);
+    echo 'Выберите операцию для выполнения: ' . PHP_EOL;
+
+    foreach ($operations as $operation) {
+        if($isItemsEmpty && mb_stristr($operation, (string)OPERATION_DELETE, true) !== false) {
+            continue;
+        }
+        echo $operation . PHP_EOL;
+    }
+    echo PHP_EOL . '> ';
+    return trim(fgets(STDIN));
+}
+
+function CheckOperationNumber ($operationNumber, $operations): bool
+{
+    if(array_key_exists($operationNumber, $operations)){
+        echo 'Выбрана операция: '  . $operations[$operationNumber] . PHP_EOL;
+        return true;
+    }
+    system('cls');
+    echo '!!! Неизвестный номер операции, повторите попытку.' . PHP_EOL;
+    return false;
+}
+
+function AddItem (&$items, &$isItemsEmpty): void
+{
+    echo "Введение название товара для добавления в список: \n> ";
+    $itemName = trim(fgets(STDIN));
+    if($itemName !== ""){
+        $items[] = $itemName;
+        SetIsItemsEmpty($items, $isItemsEmpty);
+        ShowShoppingList($items, $isItemsEmpty);
+    }
+}
+
+function DeleteItem (&$items, &$isItemsEmpty): bool
+{
+    if($isItemsEmpty){
+        echo "Ваш список покупок пуст, удалять не чего!" . PHP_EOL;
+        return false;
+    }
+    // Проверить, есть ли товары в списке? Если нет, то сказать об этом и попросить ввести другую операцию
+    echo 'Введение название товара для удаления из списка:' . PHP_EOL . '> ';
+    $itemName = trim(fgets(STDIN));
+
+    if (in_array($itemName, $items, true) !== false) {
+        while (($key = array_search($itemName, $items, true)) !== false) {
+            unset($items[$key]);
+        }
+        echo "\nТовар <" . $itemName ."> удален из списка" . PHP_EOL;
+        ShowShoppingList($items, $isItemsEmpty);
+    }else{
+        echo "Товара с названием <" . $itemName . "> в списке нет!" . PHP_EOL;
+        return false;
+    }
+    SetIsItemsEmpty($items, $isItemsEmpty);
+    return true;
+}
+
+function SetIsItemsEmpty ($items, &$isItemsEmpty): void
+{
+    if(count($items) === 0) {
+        $isItemsEmpty = true;
+        return;
+    }
+    $isItemsEmpty = false;
+}
